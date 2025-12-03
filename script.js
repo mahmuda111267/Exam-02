f12804ce3eb9abf9c81a1385d2969abd5468905a
Mahmuda <me.mujahiderfan@gmail.com>
2025-12-03 15:32:05 +0600

    First Problem

---
+++ script.js
@@ -0,0 +1,14 @@
+// 1. Coffee Shop Billing
+let coffee = 120;
+let cost = coffee * 3;
+if (cost >= 360){
+    let taxt = cost *.18 ;
+    let total = cost + taxt;
+    console.log(total);
+}
+else{
+    console.log("taxt dite hobe na")
+}
+
+
+
